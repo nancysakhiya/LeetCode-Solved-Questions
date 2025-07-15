83a74ed6e1881b724b8ad8a53b625ec3aeed29f4
nancysakhiya <152253795+nancysakhiya@users.noreply.github.com>
2025-07-15 11:57:02 +0530

    Time: 0 ms (100%), Space: 17.6 MB (98.01%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        if len(word) < 3:
+            return False
+
+        vowel = False
+        consonant = False
+
+        for c in word:
+            if c.isalpha():
+                if c.lower() in "aeiou":
+                    vowel = True
+                else:
+                    consonant = True
+                
+            elif not c.isdigit():
+                return False
+
+        return vowel and consonant
