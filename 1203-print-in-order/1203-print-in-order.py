208acbeb3153199d8bffdbbc005012862bd76da4
nancysakhiya <152253795+nancysakhiya@users.noreply.github.com>
2025-07-23 15:40:16 +0530

    Time: 55 ms (48.08%), Space: 18.1 MB (74.17%) - LeetHub

---
+++ 1203-print-in-order/1203-print-in-order.py
@@ -0,0 +1,26 @@
+from threading import Event
+class Foo:
+    def __init__(self):
+        self.done = (Event(), Event())
+
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.done[0].set()
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        
+        # printSecond() outputs "second". Do not change or remove this line.
+        self.done[0].wait()
+        printSecond()
+        self.done[1].set()
+
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        
+        # printThird() outputs "third". Do not change or remove this line.
+        self.done[1].wait()
+        printThird()
