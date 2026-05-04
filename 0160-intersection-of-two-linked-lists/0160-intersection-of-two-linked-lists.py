91d461b997f62c32060bdc3795a6c397a740efa6
nancysakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-05-04 11:42:52 +0530

    Time: 117 ms (33.9%), Space: 38.1 MB (51.62%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -5,35 +5,24 @@
 #         self.next = None
 
 class Solution:
-    def collisionPoint(self, t1: ListNode, t2: ListNode, d: int):
-        while d:
-            d -= 1
-            t2 = t2.next
-            
+
+    def getIntersectionNode(self, head1: ListNode, head2: ListNode) -> Optional[ListNode]:
+        if head1 is None or head2 is None:
+            return None
+
+        t1 = head1
+        t2 = head2
+
         while t1 != t2:
             t1 = t1.next
             t2 = t2.next
-            
+
+            if t1 == t2:
+                return t1
+
+            if t1 is None:
+                t1 = head2
+            if t2 is None:
+                t2 = head1
+
         return t1
-
-    def getIntersectionNode(self, head1: ListNode, head2: ListNode) -> Optional[ListNode]:
-        t1 = head1
-        n1 = 0
-        t2 = head2
-        n2 = 0
-        
-        while t1 is not None:
-            n1 += 1
-            t1 = t1.next
-            
-        while t2 is not None:
-            n2 += 1
-            t2 = t2.next
-            
-        if n1 < n2:
-            return self.collisionPoint(head1, head2, n2 - n1)
-            
-        else:
-            return self.collisionPoint(head2, head1, n1 - n2)
-            
-
