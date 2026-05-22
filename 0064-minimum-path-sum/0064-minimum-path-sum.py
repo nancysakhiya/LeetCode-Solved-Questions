7c87ce1af6ae42760f6370abaef52484019cc903
nancysakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-05-22 19:19:32 +0530

    Time: 31 ms (18.18%), Space: 21.7 MB (49.49%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,12 +1,24 @@
 class Solution:
-    def minPathSum(self, grid: List[List[int]]) -> int:
-        row, col = len(grid), len(grid[0])
-
-        res = [[float("inf")] * (col + 1) for r in range(row + 1)]
-        res[row - 1][col] = 0
-
-        for r in range(row - 1, -1, -1):
-            for c in range(col - 1, -1, -1):
-                res[r][c] = grid[r][c] + min(res[r + 1][c], res[r][c + 1])
+    def helper(self, i, j, grid, dp):
+        if i == 0 and j == 0:
+            return grid[0][0]
+            
+        if i < 0 or j < 0:
+            return float('inf')
+            
+        if dp[i][j] != -1:
+            return dp[i][j]
+            
+        up = grid[i][j] + self.helper(i - 1, j, grid, dp)
+        left = grid[i][j] + self.helper(i, j - 1, grid, dp)
         
-        return res[0][0]
+        dp[i][j] = min(up, left)
+        
+        return dp[i][j]
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
+        
+        return self.helper(m - 1, n - 1, grid, dp)
