e3950797d1c202b8cdfb30744a476fa57bbf7597
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-07-13 13:41:57 +0530

    Time: 477 ms (30.41%), Space: 21.3 MB (49.85%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -0,0 +1,32 @@
+class Solution:
+    def lcs(self, s1, s2):
+        # code here
+        n = len(s1)
+        m = len(s2)
+        
+        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+        
+        for j in range(m):
+            dp[0][j] = 0
+            
+        for i in range(n):
+            dp[i][0] = 0
+            
+        for idx1 in range(1, n + 1):
+            for idx2 in range(1, m + 1):
+                if s1[idx1 - 1] == s2[idx2 - 1]:
+                    dp[idx1][idx2] = 1 + dp[idx1 - 1][idx2 - 1]
+            
+                else:
+                    dp[idx1][idx2] = max(dp[idx1 - 1][idx2], dp[idx1][idx2 - 1])
+        
+        return dp[n][m]
+        
+    def longestPalinSubseq(self, s):
+        
+        return self.lcs(s, s[::-1])
+        # code here
+    def minInsertions(self, s: str) -> int:
+
+        return len(s) - self.longestPalinSubseq(s)
+        
