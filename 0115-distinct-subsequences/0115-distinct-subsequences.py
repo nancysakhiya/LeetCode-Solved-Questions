8525a8b8f6a966d466421dab0dfbf2c77426285b
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-07-14 12:22:13 +0530

    Time: 451 ms (39.8%), Space: 28.5 MB (66.58%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,14 +1,14 @@
 class Solution:
     def helper(self, i, j, s, t, dp):
-        if j < 0:
+        if j == 0:
             return 1
-        if i < 0:
+        if i == 0:
             return 0
 
         if dp[i][j] != -1:
             return dp[i][j]
 
-        if s[i] == t[j]:
+        if s[i - 1] == t[j - 1]:
             dp[i][j] = self.helper(i-1, j-1, s, t, dp) + self.helper(i-1, j, s, t, dp)
         else:
             dp[i][j] = self.helper(i - 1, j, s, t, dp)
@@ -19,6 +19,6 @@ class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         n = len(s)
         m = len(t)
-        dp = [[-1 for _ in range(m)] for _ in range(n)]
+        dp = [[-1 for _ in range(m + 1)] for _ in range(n + 1)]
 
-        return self.helper(n-1, m-1, s, t, dp)
+        return self.helper(n, m, s, t, dp)
