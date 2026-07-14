b5ba981859d8b7c1d3ad1a70848b55bc25be9ee0
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-07-14 12:30:24 +0530

    Time: 450 ms (39.95%), Space: 76.6 MB (27.46%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,24 +1,22 @@
 class Solution:
-    def helper(self, i, j, s, t, dp):
-        if j == 0:
-            return 1
-        if i == 0:
-            return 0
-
-        if dp[i][j] != -1:
-            return dp[i][j]
-
-        if s[i - 1] == t[j - 1]:
-            dp[i][j] = self.helper(i-1, j-1, s, t, dp) + self.helper(i-1, j, s, t, dp)
-        else:
-            dp[i][j] = self.helper(i - 1, j, s, t, dp)
-
-        return dp[i][j]
-
-        
     def numDistinct(self, s: str, t: str) -> int:
         n = len(s)
         m = len(t)
-        dp = [[-1 for _ in range(m + 1)] for _ in range(n + 1)]
+        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
 
-        return self.helper(n, m, s, t, dp)
+        for i in range(n + 1):
+            dp[i][0] = 1
+        for j in range(1, m + 1):
+            dp[0][j] = 0
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+
+        return dp[n][m]
+
+
+        
