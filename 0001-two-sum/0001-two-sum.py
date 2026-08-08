b4d8754fc3dd6eff2debba1d7f59d3ddae61c695
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-08-08 11:43:20 +0530

    Time: 0 ms (100%), Space: 20.5 MB (40.56%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        mpp = {}
+
+        # a + b = target
+        for i in range(n):
+            a = nums[i]
+            b = target - a
+
+            if b in mpp:
+                return [mpp[b], i]
+
+            mpp[a] = i
+
+        return []
