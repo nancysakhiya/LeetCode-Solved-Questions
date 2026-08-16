aa617de17ac3fefdaeb78e490496cc043d66fa5e
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-08-16 23:24:56 +0530

    Time: 1489 ms (19.47%), Space: 20.4 MB (84.91%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longestPrefix(self, s: str) -> str:
+        
+        for i in range(len(s) - 1, 0, -1):
+            if s[:i] == s[len(s) - i:]:
+                return s[:i]
+
+        return ""
