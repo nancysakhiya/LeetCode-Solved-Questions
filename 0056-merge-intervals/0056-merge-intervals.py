d7c318a1db00172336508cf4c060f523ca87092f
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-08-21 11:49:20 +0530

    Time: 7 ms (72.23%), Space: 23.1 MB (38.74%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,14 +1,22 @@
 class Solution:
     def merge(self, arr: List[List[int]]) -> List[List[int]]:
-        arr.sort(key = lambda i : i[0])
+        arr.sort()
         n = len(arr)
         ans = []
 
         for i in range(n):
-            if not ans or arr[i][0] > ans[-1][1]:
-                ans.append(arr[i])
-            else:
-                ans[-1][1] = max(ans[-1][1], arr[i][1])
+            start = arr[i][0]
+            end = arr[i][1]
+
+            if ans and end <= ans[-1][1]:
+                continue
+
+            for j in range(i + 1, n):
+                if arr[j][0] <= end:
+                    end = max(end, arr[j][1])
+                else:
+                    break
+
+            ans.append([start, end])
 
         return ans
-        
