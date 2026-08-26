7e769fd216af901f5b293b3e1d33af7fd4e50fcf
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-08-26 14:39:11 +0530

    Time: 5 ms (35.76%), Space: 20.4 MB (55.44%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,37 +1,36 @@
-class Solution:
-    def findOrder(self, V: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = [[] for _ in range(V)]
-        
-        for first, second in prerequisites:
-            adj[second].append(first)
-            
-        indegree = [0] * V
+from collections import deque
 
+class Solution:
+    def findOrder(self, V: int, pre: List[List[int]]) -> List[int]:
+        adj = [[] for _ in range(V)]
+        for v, u in pre:
+            adj[u].append(v)
+
+        # calculating indegree
+        indegree = [0] * V
         for i in range(V):
             for it in adj[i]:
                 indegree[it] += 1
 
         q = deque()
-
         for i in range(V):
             if indegree[i] == 0:
                 q.append(i)
 
         topo = []
-
         while q:
             node = q.popleft()
             topo.append(node)
 
+            # the node is in the topo so we remove it from the indegree
             for it in adj[node]:
                 indegree[it] -= 1
-
                 if indegree[it] == 0:
                     q.append(it)
-                
-        
-        if len(topo) == V:
-            return topo
-            
-        return []
+
+        if len(topo) != V:
+            return []
+
+        return topo
+
         
