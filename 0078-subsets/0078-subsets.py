96644e55d51bfada4586fc6a2b6387a11fc2a3bc
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-08-29 16:18:02 +0530

    Time: 0 ms (100%), Space: 19 MB (99.83%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,5 +1,6 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        # we used bit manipulation to find subsets
         n = len(nums)
         subset = 1 << n # this is basically 2^n. because we are iterating from 0 to subset
         ans = []
