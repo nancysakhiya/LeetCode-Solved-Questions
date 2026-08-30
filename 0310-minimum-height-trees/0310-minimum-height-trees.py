88dafc00901fbb096026431162e564c9ca02b764
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-08-30 15:50:50 +0530

    Time: 55 ms (40.28%), Space: 27.5 MB (98.71%) - LeetHub

---
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -0,0 +1,37 @@
+from collections import deque
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n == 1:
+            return [0]
+        adj = [[] for _ in range(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        # we first try to find leaf nodes. 
+        # leaf node has excatly one neighbour. so the len of adj list of leaf is 1
+        # we will use queue to store all the leaf nodes
+        q = deque()
+        for i in range(n):
+            if len(adj[i]) == 1:
+                q.append(i)
+
+        remaining = n
+
+        # now we remove leaf nodes layer by layer
+        # a tree can either have one center or 2 center
+        while remaining > 2:
+            size = len(q)
+
+            for i in range(size):
+                leaf = q.popleft()
+
+                for it in adj[leaf]:
+                    adj[it].remove(leaf)
+
+                    if len(adj[it]) == 1:
+                        q.append(it)
+
+            remaining -= size
+
+        return list(q)
