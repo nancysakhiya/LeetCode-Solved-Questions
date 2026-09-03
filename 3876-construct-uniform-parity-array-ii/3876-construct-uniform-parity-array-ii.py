325153dd187d08951ec43fd2d75ad3b2f0cffeea
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-09-03 09:22:19 +0530

    Time: 108 ms (21.95%), Space: 35.4 MB (51.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -4,20 +4,16 @@ class Solution:
 
         minodd = float('inf')
 
-        # we will find the smallest oddd number and we will count all the odd number
         for i in range(n):
             if nums[i] % 2 != 0:
                 minodd = min(minodd, nums[i])
-        
-        # if there is no odd number everything is even, we can return
+
         if minodd == float('inf'):
             return True
 
-        # Case 2: if only one number is odd
         for i in range(n):
             if nums[i] % 2 == 0:
-                if nums[i] <= minodd:
+                if nums[i] < minodd:
                     return False
 
         return True
-        
