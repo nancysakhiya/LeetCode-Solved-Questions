2b60149af7c80955a83b7313959350a96454c624
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-09-06 10:35:27 +0530

    Time: 432 ms (43.33%), Space: 28.5 MB (66.2%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,25 +1,25 @@
 class Solution:
+    def recursion(self, i, j, dp, s, t):
+        if j < 0:
+            return 1
+        if i < 0:
+            return 0
+
+        if dp[i][j] != -1:
+            return dp[i][j]
+
+        if s[i] == t[j]:
+            dp[i][j] = self.recursion(i - 1, j - 1, dp, s, t) + self.recursion(i - 1, j, dp, s, t)
+        else:
+            dp[i][j] = self.recursion(i - 1, j, dp, s, t)
+
+        return dp[i][j]
+
     def numDistinct(self, s: str, t: str) -> int:
         n = len(s)
         m = len(t)
 
-        prev = [0] * (m + 1)
-        
+        dp = [[-1 for _ in range(m + 1)] for _ in range(n + 1)]
 
-        prev[0] = 1
-
-        for i in range(1, n + 1):
-            cur = [0] * (m + 1)
-            cur[0] = 1
-            for j in range(1, m + 1):
-                if s[i - 1] == t[j - 1]:
-                    cur[j] = prev[j-1] + prev[j]
-                else:
-                    cur[j] = prev[j]
-
-            prev = cur
-
-        return prev[m]
-
-
+        return self.recursion(n - 1, m - 1, dp, s, t)
         
