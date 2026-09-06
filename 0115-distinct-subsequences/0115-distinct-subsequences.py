27c5301993e30f1a1023cf8ddc98d4989084b289
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-09-06 10:48:12 +0530

    Time: 465 ms (35.73%), Space: 76.8 MB (23.42%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,25 +1,20 @@
 class Solution:
-    def recursion(self, i, j, dp, s, t):
-        if j == 0:
-            return 1
-        if i == 0:
-            return 0
-
-        if dp[i][j] != -1:
-            return dp[i][j]
-
-        if s[i - 1] == t[j - 1]:
-            dp[i][j] = self.recursion(i - 1, j - 1, dp, s, t) + self.recursion(i - 1, j, dp, s, t)
-        else:
-            dp[i][j] = self.recursion(i - 1, j, dp, s, t)
-
-        return dp[i][j]
-
     def numDistinct(self, s: str, t: str) -> int:
         n = len(s)
         m = len(t)
 
-        dp = [[-1 for _ in range(m + 1)] for _ in range(n + 1)]
+        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
 
-        return self.recursion(n, m, dp, s, t)
+        for i in range(n):
+            dp[i][0] = 1
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+
+        return dp[n][m]
         
