6f3cea0082a5b07a36c327a06dad2f45741e2dbf
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-09-12 17:28:31 +0530

    Time: 0 ms (100%), Space: 19.5 MB (20.38%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -0,0 +1,21 @@
+import heapq
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        mpp = {}
+        for word in words:
+            mpp[word] = mpp.get(word, 0) + 1
+
+        heap = []
+        for word, freq in mpp.items():
+            heapq.heappush(heap, (-freq, word))
+
+        ans = []
+        for i in range(k):
+            freq, word = heapq.heappop(heap)
+            ans.append(word)
+
+        return ans
+
+        
+
+        
