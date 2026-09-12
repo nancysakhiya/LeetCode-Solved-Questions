8abc2b9b868ffe8b52892aaa443fb342227c460a
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-09-12 16:54:11 +0530

    Time: 2 ms (13.17%), Space: 19.4 MB (70.73%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,24 +1,21 @@
 import heapq
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        n = len(stones)
         heap = []
 
-        for i in range(n):
-            heapq.heappush(heap, -stones[i])
+        for stone in stones:
+            heapq.heappush(heap, -stone) # maxheap in python
 
         while len(heap) > 1:
-            num2 = -heapq.heappop(heap) # largest element
-            num1 = -heapq.heappop(heap) # second largest element from the heap
-            
+            # first we get largest element
+            # then we get second largest element
+            y = -heapq.heappop(heap)
+            x = -heapq.heappop(heap)
 
-            if num1 != num2:
-                heapq.heappush(heap, -(num2 - num1)) # because x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x
+            if x != y:
+                heapq.heappush(heap, -(y - x))
 
         if heap:
             return -heap[0]
 
         return 0
-
-
-        
