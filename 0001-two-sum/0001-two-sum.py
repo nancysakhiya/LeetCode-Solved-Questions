9b22fd17a17501ab90a4171eac0e9701c4823921
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-09-15 19:02:41 +0530

    Time: 3 ms (53.51%), Space: 20.4 MB (59.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
+
         mpp = {}
 
         for i in range(n):
             a = nums[i]
-            b = target - a
+            b = target - nums[i]
 
             if b in mpp:
                 return (mpp[b], i)
