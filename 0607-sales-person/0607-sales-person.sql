888bde733d01fb74ccd26f69764ddb6c7d621ec5
Nancy Sakhiya <152253795+nancysakhiya@users.noreply.github.com>
2026-09-24 22:44:58 +0530

    Time: 2940 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select s.name from SalesPerson s
+where not exists (
+    select 1 from Orders o 
+    join Company c on o.com_id = c.com_id
+    where o.sales_id = s.sales_id and c.name = 'RED'
+)
